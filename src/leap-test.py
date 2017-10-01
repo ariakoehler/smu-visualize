dc55bcb9e4d7c79e21ed4b03a2808764cde1c245
Carter Koehler <carterleekoehler@gmail.com>
2017-10-01 00:00:29 -0500

    Updated Interface

--- src/leap-test.py
+++ src/leap-test.py
@@ -9,12 +9,36 @@ import Leap
 
 class SampleListener(Leap.Listener):
 
+    recent_hand = None
+    recent_frame = None
+
     def on_connect(self, controller):
         print('Connected')
 
     def on_frame(self, controller):
-        print('Frame available')
+        # Check that frame was retrieved
+        print('Frame available.')
+
+        # get new frame and log it in member data
+        frame = controller.frame()
+        self.recent_frame = frame
+
+        # get hands in frame, update most recent hand if only one
+        handlist = frame.hands
+        if len(handlist) is 1 and handlist[0].id is not self.recent_hand:
+            self.recent_hand = handlist[0].id
+            
+        if self.recent_frame is not None and self.recent_hand is not None:
+            print(self.recent_frame.hand(self.recent_hand).palm_position, self.recent_frame.hand(self.recent_hand).palm_normal)
         
+        # for hand in handlist:
+        #     print('{}, {}'.format(hand.palm_position, hand.palm_normal))
+
+    def get_hand_data(self):
+        if self.recent_frame is not None and self.recent_hand is not None:
+            return self.recent_frame.hand(recent_hand).palm_position, self.recent_frame.hand(recent_hand).palm_normal
+        else:
+            return -1, -1
 
 
 def main():
